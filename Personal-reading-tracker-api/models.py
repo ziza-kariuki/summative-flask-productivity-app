1f3aa897e72edcf169b512f0278e77f560725a2c
ZizaKariuki <zizawabetti@gmail.com>
2026-09-10 14:15:11 +0300

    Creating Reading Entry model

--- Personal-reading-tracker-api/models.py
+++ Personal-reading-tracker-api/models.py
@@ -17,3 +17,20 @@ class User(db.Model):
     )
     def __repr__(self):
         return f'<User {self.username}>'
+
+class ReadingEntry(db.Model):
+    __tablename__ = 'reading_entries'
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False, default='want_to_read')
+    rating = db.Column(db.Integer, nullable=True)
+    start_date = db.Column(db.Date, nullable=True)
+    completion_date = db.Column(db.Date, nullable=True)
+ 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', back_populates='reading_entries')
+ 
+    def __repr__(self):
+        return f'<ReadingEntry {self.title!r} by {self.author!r}>'
